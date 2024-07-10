36355f24595f37122034d36d59a67e92405b621e
minss7 <paul2723@snu.ac.kr>
2024-07-10 14:40:50 +0900

    FEAT: 과별페이지 title & background 추가 western painting text와 background 구현

--- src/pages/DepartmentDetail.jsx
+++ src/pages/DepartmentDetail.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const TitleBackground = styled.div`
+  background-color: #ee5397;
+  width: 1440px;
+  height: 231px;
+`;
+
+const TitleText = styled.div`
+  position: fixed;
+  font-family: "Pretendard-ExtraBold", Helvetica;
+  font-weight: 800;
+  color: #ffffff;
+  font-size: 70px;
+  letter-spacing: 0;
+  line-height: 70px;
+  padding: 55px 818px 106px 45px;
+`;
 
 function DepartmentDetail() {
-  return <div>과별 페이지입니다.</div>;
+  return (
+    <>
+      <TitleBackground>
+        <TitleText>Western Painting</TitleText>
+      </TitleBackground>
+    </>
+  );
 }
 
 export default DepartmentDetail;
