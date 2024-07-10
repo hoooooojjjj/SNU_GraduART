6454c3ea8c9b65438c5baf827cda7b99d7b68673
minss7 <paul2723@snu.ac.kr>
2024-07-10 20:04:22 +0900

    중간점검

---
+++ src/pages/DepartmentDetail/DepartmentDetailStyle.js
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+
+export const Contaioner = styled.div`
+  width: 100dvw;
+  height: auto;
+`;
+export const Intro = styled.div`
+  width: 100%;
+  height: 100dvh;
+`;
+export const TitleBackground = styled.div`
+  background-color: #ee5397;
+  width: 100%;
+  height: 29%;
+`;
+export const TitleText = styled.div`
+  font-family: "Pretendard-ExtraBold", Helvetica;
+  font-weight: 800;
+  color: #ffffff;
+  font-size: 70px;
+  position: relative;
+  top: 24%;
+  left: 3%;
+`;
+export const Img = styled.img`
+  width: 100%;
+  height: 71%;
+  background-size: cover;
+`;
+
+export const DescriptionExtra = styled.div`
+  width: 348px;
+`;
+
+export const DescriptionText = styled.div`
+  height: 291px;
+  width: 729px;
+  color: #000000;
+  font-family: "Pretendard-Regular", Helvetica;
+  font-size: 24px;
+  font-weight: 400;
+`;
+
+export const DescriptionContainer = styled.div`
+  width: 100dvw;
+  height: 61dvh;
+`;
