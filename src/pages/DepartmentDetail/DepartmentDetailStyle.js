50a8c67276ab822bd80b5bc76b47d0bf39e70b5f
minss7 <paul2723@snu.ac.kr>
2024-07-10 22:07:43 +0900

    FEAT: 서양화 설명

--- src/pages/DepartmentDetail/DepartmentDetailStyle.js
+++ src/pages/DepartmentDetail/DepartmentDetailStyle.js
@@ -1,47 +1,88 @@
 import styled from "@emotion/styled";
 
-export const Contaioner = styled.div`
+export const Container = styled.div`
+  width: 100dvw;
+  height: auto;
+  background-color: #ffffff;
+`;
+export const IntroContainer = styled.div`
   width: 100dvw;
   height: auto;
 `;
+
 export const Intro = styled.div`
   width: 100%;
   height: 100dvh;
 `;
+
+export const Header = styled.div`
+  width: 100%;
+  height: 10dvh;
+  background-color: #ffffff;
+`;
+
 export const TitleBackground = styled.div`
   background-color: #ee5397;
   width: 100%;
-  height: 29%;
+  height: 25dvh;
 `;
+
+export const Img = styled.img`
+  width: 100%;
+  height: 65dvh;
+  background-size: cover;
+`;
+
 export const TitleText = styled.div`
   font-family: "Pretendard-ExtraBold", Helvetica;
   font-weight: 800;
   color: #ffffff;
   font-size: 70px;
+  width: 97%;
   position: relative;
   top: 24%;
   left: 3%;
 `;
-export const Img = styled.img`
+
+export const DescriptionContainer = styled.div`
   width: 100%;
-  height: 71%;
-  background-size: cover;
+  height: 60dvh;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  position: relative;
+`;
+
+export const MiddleContainer = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  justify-content: space-between;
+  width: 88%;
+  height: 75%;
 `;
 
 export const DescriptionExtra = styled.div`
-  width: 348px;
+  width: 27%;
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  color: #000000;
+`;
+
+export const DescriptionBox = styled.div`
+  height: 291px;
+  width: 58%;
+  background-color: #ffffff;
+  position: relative;
 `;
 
 export const DescriptionText = styled.div`
-  height: 291px;
-  width: 729px;
+  position: absolute;
+  top: 9%;
+  width: 100%;
   color: #000000;
   font-family: "Pretendard-Regular", Helvetica;
   font-size: 24px;
   font-weight: 400;
-`;
-
-export const DescriptionContainer = styled.div`
-  width: 100dvw;
-  height: 61dvh;
 `;
