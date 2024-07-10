e444216df8c893e3803ef75be58bf7eb8c11e4e7
Daeyu <paull07@snu.ac.kr>
2024-07-11 02:13:25 +0900

    Feat : Header Basic Form
    Header Buttons Placement

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled'
+export const Head = styled.header`
+    background-color: transparent;
+    padding: 10px;
+    color: lightgray;
+`
+
+export const List = styled.ul`
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    align-items: center;
+    gap: 3%;
+    margin: 0 20px 0 0;
+`
+
+export const HeaderIcons = styled.li`
+    width : 23px;
+    height : 23px;
+`
+
+export const NavBar = styled.nav`
+    margin-top: 10px;
+`
+
+export const ListItem = styled.li`
+    font-weight: normal;
+    font-size: medium;
+`
+
+export const Seperator = styled.div`
+    margin-right: 3%;
+`
