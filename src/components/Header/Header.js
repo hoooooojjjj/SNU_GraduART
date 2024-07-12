84f08de59cac55d8e7823ade6cc0ef1dc8f07c22
Daeyu <paull07@snu.ac.kr>
2024-07-13 03:51:14 +0900

    Fix : Transparent Background
    Background and Ratio changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import styled from '@emotion/styled'
-import {Link, NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import React from "react";
 export const Head = styled.header`
-    background-color: darkgrey;  //랜딩페이지에 붙일 때는 transparent;
-    padding: 10px;
-    color: lightgray;
+    position: absolute;
+    margin-top: 0.5dvw;
+    margin-left: 15dvw;
+    background: transparent;
+    color: white;
+    z-index: 10; //꼭대기에 올리기
 `
 export const List = styled.ul`
     list-style-type: none;
