10771c118097fb188b2ac65aca21b9e3aa6f0a0f
sono <rhj080471@gmail.com>
2024-07-23 20:02:16 +0900

    Feat : 장바구니 페이지 리액트 쿼리로 fetch 최적화
    useQuery()로 데이터 가져오기
    useMutation()로 데이터 삭제하기

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -25,12 +25,42 @@ import { userContext } from "../../App.jsx";
 import { supabase } from "../../ServerClient.js";
 import { isMobile } from "react-device-detect";
 import { useNavigate } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+// 해당 유저가 담은 장바구니 리스트 가져오는 함수
+const getUserCartItemList = async (user) => {
+  let { data: cart_item, error } = await supabase
+    .from("cart_item")
+    .select("*")
+    // 해당 유저의 상품만 필터링
+    .eq("user_id", user.id)
+    .order("created_at", { ascending: false });
+  if (error) {
+    console.log(error);
+    throw new Error(error.message);
+    // Error handling 추가
+  }
+  return cart_item;
+};
+
+// 장바구니에서 삭제하는 함수
+const deleteItem = async (item) => {
+  const { error } = await supabase
+    .from("cart_item")
+    .delete()
+    .eq("item_id", item.item_id);
+  if (error) {
+    console.log(error);
+    throw new Error(error.message);
+  }
+};
 
 function Cart() {
-  const nav = useNavigate();
+  const queryClient = useQueryClient();
 
-  // 유저가 장바구니에 담은 작품 리스트
-  const [userCartItemList, setUserCartItemList] = useState([]);
+  const nav = useNavigate();
 
   // 유저 정보 가져오기
   const [user] = useContext(userContext);
@@ -42,108 +72,56 @@ function Cart() {
   const [isCheckedAll, setIsCheckedAll] = useState(false);
 
   // 해당 유저가 담은 장바구니 리스트 가져오기
-  useEffect(() => {
-    const getUserCartItemList = async () => {
-      let { data: cart_item, error } = await supabase
-        .from("cart_item")
-        .select("*")
-        // 해당 유저의 상품만 필터링
-        .eq("user_id", user.id)
-        .eq("onSale", true)
-        .order("created_at", { ascending: false });
-      if (error) {
-        console.log(error);
-      }
-      setUserCartItemList(cart_item);
-    };
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: [`getUserCartItemList`, user],
+    queryFn: () => getUserCartItemList(user),
+  });
 
-    if (user) {
-      getUserCartItemList();
-    }
-  }, [user]);
-
-  // 로컬 스토리지에서 선택된 아이템 id 복원
-  useEffect(() => {
-    const savedSelectedItemIds = JSON.parse(
-      localStorage.getItem("selectedItems")
-    );
-    const savedIsCheckedAll = JSON.parse(localStorage.getItem("isCheckedAll"));
-    if (savedSelectedItemIds) {
-      const savedSelectedItems = userCartItemList.filter((item) =>
-        savedSelectedItemIds.includes(item.item_id)
-      );
-      setSelectedItems(savedSelectedItems);
-    }
-    if (savedIsCheckedAll !== null) {
-      setIsCheckedAll(savedIsCheckedAll);
-    }
-  }, [userCartItemList]);
-
-  // 선택된 아이템 상태가 변경될 때마다 로컬 스토리지에 저장
-  useEffect(() => {
-    const selectedIds = selectedItems.map((item) => item.item_id);
-    localStorage.setItem("selectedItems", JSON.stringify(selectedIds));
-    localStorage.setItem("isCheckedAll", JSON.stringify(isCheckedAll));
-  }, [selectedItems, isCheckedAll]);
-
-  // selectedItems가 변경될 때 isCheckedAll 상태를 업데이트
-  useEffect(() => {
-    if (
-      selectedItems.length === userCartItemList.length &&
-      userCartItemList.length > 0
-    ) {
-      setIsCheckedAll(true);
-    } else {
-      setIsCheckedAll(false);
-    }
-  }, [selectedItems, userCartItemList]);
-
-  // 삭제하기 버튼 클릭 시 해당 작품 cart_item 테이블에서 삭제
-  const handleCartItemDelete = async (item) => {
-    if (user) {
-      const { error } = await supabase
-        .from("cart_item")
-        .delete()
-        .eq("item_id", item.item_id);
-      if (error) {
-        console.log(error);
-      }
-    }
-
-    const newCartItemList = userCartItemList.filter(
-      (cartItem) => cartItem.item_id !== item.item_id
-    );
-    setUserCartItemList(newCartItemList);
-  };
+  // 작품 삭제하는 mutation
+  const mutation = useMutation({
+    // 작품 삭제하는 함수
+    mutationFn: deleteItem,
+    // 작품 삭제가 성공되었을 때
+    onSuccess: () => {
+      // 해당 작품을 삭제하고 나서 cart_item 테이블을 다시 fetch
+      queryClient.invalidateQueries("getUserCartItemList");
+    },
+    onError: (error) => {
+      console.error("Error deleting item:", error.message);
+    },
+  });
 
   // 전체 작품 선택 클릭 시
   const handleCheckAll = () => {
-    // 만약 다 체크되어 있었다면 선택된 작품 리스트를 비움
-    if (isCheckedAll) {
-      setSelectedItems([]);
-    } else {
-      setSelectedItems(userCartItemList);
+    if (data) {
+      // 만약 다 체크되어 있었다면 선택된 작품 리스트를 비움
+      if (isCheckedAll) {
+        setSelectedItems([]);
+      } else {
+        setSelectedItems(data);
+      }
+      setIsCheckedAll(!isCheckedAll);
     }
-    setIsCheckedAll(!isCheckedAll);
   };
 
   // 체크박스가 변경될 때마다 선택된 작품을 업데이트
   const handleCheckboxChange = (event, item) => {
-    // 체크를 눌렀으면 선택된 작품 리스트에 추가,
     if (event.target.checked) {
-      setSelectedItems([...selectedItems, item]);
+      setSelectedItems((prevSelectedItems) => {
+        const updatedItems = [...prevSelectedItems, item];
+        return updatedItems;
+      });
     } else {
-      // 체크를 해제했으면 선택된 작품 리스트에서 제거
-      setSelectedItems(
-        selectedItems.filter(
+      setSelectedItems((prevSelectedItems) => {
+        const updatedItems = prevSelectedItems.filter(
           (selectedItem) => selectedItem.item_id !== item.item_id
-        )
-      );
+        );
+        return updatedItems;
+      });
     }
   };
 
   // 선택된 작품 주문하기
-
   const orderSelectedItems = async (userId, itemIds) => {
     try {
       const response = await fetch(
@@ -154,20 +132,15 @@ function Cart() {
             "Content-Type": "application/json",
             Authorization: `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
           },
-          body: JSON.stringify({
-            user_id: userId,
-            item_ids: itemIds,
-          }),
+          body: JSON.stringify({ user_id: userId, item_ids: itemIds }),
         }
       );
-
       if (!response.ok) {
         const errorData = await response.json();
         throw new Error(errorData.error);
       }
-
-      const data = await response.json();
-      return data;
+      const orderData = await response.json();
+      return orderData;
     } catch (error) {
       console.error("Error ordering items:", error);
       throw error;
@@ -183,29 +156,101 @@ function Cart() {
       console.log("Order successful:", response);
       const tid = response.tid;
       window.localStorage.setItem("temp_tid", tid);
-
       if (isMobile) {
         window.location.assign(`${response.next_redirect_mobile_url}`);
       } else {
         window.location.assign(`${response.next_redirect_pc_url}`);
       }
-
       setSelectedItems([]);
       setIsCheckedAll(false);
-      setRenderKey(renderKey + 1); // Refresh cart items
+      setRenderKey(renderKey + 1);
+      // Refresh cart items
     } catch (error) {
       console.error("Order failed:", error);
     }
   };
 
+  // 삭제하기 버튼 클릭 시 해당 작품 cart_item 테이블에서 삭제
+  const handleCartItemDelete = (item) => {
+    if (user) {
+      if (data) {
+        mutation.mutate(item);
+      }
+    }
+  };
+
+  // 로컬 스토리지에서 선택된 아이템 id 복원
+  useEffect(() => {
+    if (data) {
+      const savedSelectedItemIds = JSON.parse(
+        localStorage.getItem("selectedItems")
+      );
+      const savedIsCheckedAll = JSON.parse(
+        localStorage.getItem("isCheckedAll")
+      );
+      if (savedSelectedItemIds) {
+        const savedSelectedItems = data.filter((item) =>
+          savedSelectedItemIds.includes(item.item_id)
+        );
+        setSelectedItems(savedSelectedItems);
+      }
+      if (savedIsCheckedAll !== null) {
+        setIsCheckedAll(savedIsCheckedAll);
+      }
+    }
+  }, [data]);
+
+  // 선택된 아이템 상태가 변경될 때마다 로컬 스토리지에 저장
+  useEffect(() => {
+    const selectedIds = selectedItems.map((item) => item.item_id);
+    localStorage.setItem("selectedItems", JSON.stringify(selectedIds));
+    localStorage.setItem("isCheckedAll", JSON.stringify(isCheckedAll));
+  }, [selectedItems, isCheckedAll]);
+
+  // selectedItems가 변경될 때 isCheckedAll 상태를 업데이트
+  useEffect(() => {
+    if (data) {
+      const allSelected =
+        data.length > 0 && selectedItems.length === data.length;
+      setIsCheckedAll(allSelected);
+    }
+  }, [selectedItems, data]);
+
+  if (isPending) {
+    return (
+      <Container>
+        <Header></Header>
+        <ContentContainer>
+          <CartText>장바구니</CartText> <ListText>목록</ListText>
+          <CartItemList>
+            <Box
+              style={{
+                width: "100%",
+                height: "50dvh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CircularProgress color="inherit" />
+            </Box>
+          </CartItemList>
+        </ContentContainer>
+      </Container>
+    );
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
   return (
     <Container>
       <Header></Header>
       <ContentContainer>
-        <CartText>장바구니</CartText>
-        <ListText>목록</ListText>
+        <CartText>장바구니</CartText> <ListText>목록</ListText>
         <CartItemList>
-          {userCartItemList.length > 0 ? (
+          {data && data.length > 0 ? (
             <CheckAllContainer>
               <CheckAllIcon
                 type="checkbox"
@@ -218,8 +263,8 @@ function Cart() {
           ) : (
             <></>
           )}
-          {userCartItemList.length > 0 ? (
-            userCartItemList.map((item) => (
+          {data && data.length > 0 ? (
+            data.map((item) => (
               <CartItem key={item.item_id}>
                 <CheckBoxIcon
                   type="checkbox"
@@ -239,9 +284,7 @@ function Cart() {
                   ></CartItemImg>
                 </CartImgContainer>
                 <CartItemText>
-                  {item.title} | {item.artist}
-                  <br></br>
-                  <br></br>
+                  {item.title} | {item.artist} <br></br> <br></br>
                   {item.price.toLocaleString()} 원
                 </CartItemText>
                 <CartItemDelete
@@ -265,7 +308,7 @@ function Cart() {
             </p>
           )}
         </CartItemList>
-        {userCartItemList.length > 0 ? (
+        {data && data.length > 0 ? (
           <>
             <PriceContainer>
               <PriceText>선택한 작품 총액</PriceText>
