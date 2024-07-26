7c345e399d6c0bc5809aa10e03625a6ca5f71f1a
Daeyu <paull07@snu.ac.kr>
2024-07-27 04:45:16 +0900

    Fix: Delete React Import

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
 import DepartmentDetail from "../pages/DepartmentDetail/DepartmentDetail";
